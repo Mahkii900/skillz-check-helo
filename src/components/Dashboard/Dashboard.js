55111ef1911fb1ffc1fdf3fbcf1821cd8c403877
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-09 15:55:46 -0600

    finished part 2 step 1

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react'
 
 export default class Dashboard extends Component {
+    state = {
+        search: '',
+        posts: [{title: 'Baa', id: 1}, {title: 'Nooo', id: 2}]
+    }
     render() {
+        const posts = this.state.posts.map((ele) => {
+            return <div key={ele.id}>
+                <div>
+                    {ele.title}
+                </div>
+                <div>
+                    {ele.username}
+                </div>
+                <div>
+                    {ele.profile_pic}
+                </div>
+            </div>
+                
+        })
         return (
             <div>
-                Dashboard
+                <div className='search-box'>
+                    <div className='search-input'>
+                        <input type="text" placeholder={'Search...'} value={this.state.search} onChange={(e) => this.setState({search: e.target.value})}/>
+                        <input type='checkbox' id='Posts?' defaultChecked/>
+                        <label htmlFor='Posts?'>Include my posts</label>
+                    </div>
+                    <div className='search-buttons'>
+                        <button>Search</button>
+                        <button>Reset</button>
+                    </div>
+                </div>
+                <div className='post-display'>
+                    {posts}
+                </div>
             </div>
         )
     }
