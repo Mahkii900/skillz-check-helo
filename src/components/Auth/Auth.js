1839d188a2624a33876d4daf32fe6785921db030
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-09 12:46:52 -0600

    setting up login page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
 
 export default class Auth extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
     render() {
         return (
             <div>
-                Auth
+                <div className='input-boxes'>
+                    <input value={this.state.username} type='text' placeholder={'Username'} onChange={(e) => this.setState({username: e.target.value})}/>
+                    <input value={this.state.password} type='password' placeholder={'Password'} onChange={(e) => this.setState({password: e.target.value})}/>
+                </div>
+                <div className='login-buttons'>
+                    <button>Login</button>
+                    <button>Register</button>
+                </div>
             </div>
         )
     }
