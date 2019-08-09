eb3b3ac58025b5fcda7defae46ec665abfc6af4c
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-09 12:40:25 -0600

    finished step 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom'
 import './App.css';
-import Auth from './components/Auth/Auth'
-import Dashboard from './components/Dashboard/Dashboard'
-import Form from './components/Form/Form'
 import Nav from './components/Nav/Nav'
-import Post from './components/Post/Post'
+import routes from './routes'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav/>
-        <Auth/>
-        <Dashboard/>
-        <Form/>
-        <Post/>
+        {this.props.location.pathname === '/' ? null: <Nav/>}
+        {routes}
       </div>
     )
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Nav extends Component {
     render() {
-        return (
+        return(
             <div>
-                Nav
+                <div className='buttons-box'>
+                    <Link to={'/dashboard'}>
+                        <button>Home</button>
+                    </Link>
+                    <Link to={'/new'}>
+                        <button>New Post</button>
+                    </Link>
+                    <Link to={'/'}>
+                        <button>Logout</button>
+                    </Link>
+                </div>
             </div>
         )
     }
