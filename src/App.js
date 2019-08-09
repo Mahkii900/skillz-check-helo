3d4bd32d5cf2c5c15297a65b2475aeb2889d0e28
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-09 10:58:08 -0600

    finished step 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Auth from './components/Auth/Auth'
+import Dashboard from './components/Dashboard/Dashboard'
+import Form from './components/Form/Form'
+import Nav from './components/Nav/Nav'
+import Post from './components/Post/Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav/>
+        <Auth/>
+        <Dashboard/>
+        <Form/>
+        <Post/>
+      </div>
+    )
+  }
 }
 
 export default App;
